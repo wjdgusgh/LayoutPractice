37c3c645e8cc36ed0120c0027c203b1661cfc7b8
wjdgusgh <wjdgusgh14@gmail.com>
2020-12-31 20:24:50 +0900

    add toast button

--- app/src/main/java/com/example/layoutpractice/MainActivity.kt
+++ app/src/main/java/com/example/layoutpractice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.layoutpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.layoutpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.activity = this@MainActivity
+
+        //setContentView(R.layout.activity_main)
+    }
+
+    fun toastBtnClick(view: View){
+        Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
     }
 }
